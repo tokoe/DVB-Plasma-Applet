e85202900dcb18ff03c80075b767edfb2a185f54
Tobias Koenig <tokoe@kde.org>
2009-07-20 17:15:03 +0200

    Don't put the german umlauts into source code

--- common/request.cpp
+++ common/request.cpp
@@ -58,13 +58,13 @@ void Request::Private::_k_finished( KJob *job )
 
     QByteArray data = mRawData;
     data.replace( "&quot;", "\"" );
-    data.replace( "&#196;", "Ä" );
-    data.replace( "&#214;", "Ö" );
-    data.replace( "&#220;", "Ü" );
-    data.replace( "&#223;", "ß" );
-    data.replace( "&#228;", "ä" );
-    data.replace( "&#246;", "ö" );
-    data.replace( "&#252;", "ü" );
+    data.replace( "&#196;", QByteArray( 1, QChar( 196 ).toLatin1() ) );
+    data.replace( "&#214;", QByteArray( 1, QChar( 214 ).toLatin1() ) );
+    data.replace( "&#220;", QByteArray( 1, QChar( 220 ).toLatin1() ) );
+    data.replace( "&#223;", QByteArray( 1, QChar( 223 ).toLatin1() ) );
+    data.replace( "&#228;", QByteArray( 1, QChar( 228 ).toLatin1() ) );
+    data.replace( "&#246;", QByteArray( 1, QChar( 246 ).toLatin1() ) );
+    data.replace( "&#252;", QByteArray( 1, QChar( 252 ).toLatin1() ) );
 
     const Parser parser;
 
